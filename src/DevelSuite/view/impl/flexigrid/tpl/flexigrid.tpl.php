2913474a9ed0139c530db28d01ccda8a0a049d1e
Georg Henkel <georg@develman.de>
2012-08-01 17:11:10 +0200

    check what is grid

--- src/DevelSuite/view/impl/flexigrid/tpl/flexigrid.tpl.php
+++ src/DevelSuite/view/impl/flexigrid/tpl/flexigrid.tpl.php
@@ -86,6 +86,7 @@
 		}
 
 		function getRequestedColumns(grid, requestColumns, multiSelection) {
+			alert(grid);
 			var resultSet = new ResultSet(multiSelection);
 			
 			if ($('.trSelected', grid).length == 0) {
@@ -99,11 +100,11 @@
 					var row = new Row(rowIndex);
 					
 					$('td', this).each(function() {
-						var cellName = $(this).attr('data-name');
+						var cellName = $(this).attr('abbr');
 						var value = $(this).children('div').html();
 
 						$.each(requestColumns, function(index, columnName) {
-							if (abbr == columnName) {
+							if (cellName == columnName) {
 								var column = new Column(columnName, data);
 								row.addColumn(column);
 							}
@@ -115,6 +116,10 @@
 
 				return resultSet;
 			}
+		}
+
+		function getColumnIndex(columnName, grid) {
+			
 		}
 	});
 /* ]]> */
