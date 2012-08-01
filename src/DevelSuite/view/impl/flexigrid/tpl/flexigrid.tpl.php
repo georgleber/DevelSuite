4388aef35f46aac48d1135d7cd1d45fa7a6ca477
Georg Henkel <georg@develman.de>
2012-08-01 19:54:06 +0200

    t

--- src/DevelSuite/view/impl/flexigrid/tpl/flexigrid.tpl.php
+++ src/DevelSuite/view/impl/flexigrid/tpl/flexigrid.tpl.php
@@ -101,7 +101,7 @@
 						var colIndex = getIndexByTitle(columnName);
 
 						if (colIndex != -1) {
-							var value = tableRow.children().html();
+							var value = tableRow.children().eq(colIndex).html();
 							console.log("columnName: " + columnName + ", value: " + value);
 	
 							var column = new Column(columnName, value);
