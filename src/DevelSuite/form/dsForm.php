3aebc40c20c66b5ff9c572b21cca491171c370f6
Georg Henkel <georg@develman.de>
2012-08-26 11:36:54 +0200

    refactor

--- src/DevelSuite/form/dsForm.php
+++ src/DevelSuite/form/dsForm.php
@@ -229,7 +229,7 @@ class dsForm {
 			$response = array();
 
 			// send JSON without errors
-			if ($this->isValid && !$this->showErrors) {
+			if ($this->isValid() && !$this->showErrors) {
 				$response["valid"] = TRUE;
 				$response["errors"] = NULL;
 			} else {
@@ -264,7 +264,7 @@ class dsForm {
 			$html .= ">\n";
 
 			// set errors
-			if (!$this->isValid || $this->showErrors) {
+			if (!$this->isValid() || $this->showErrors) {
 				// load text for form error message
 				$bundle = dsResourceBundle::getBundle(dirname(__FILE__), "form");
 				$errorText = $bundle['Form.formErrors'];
