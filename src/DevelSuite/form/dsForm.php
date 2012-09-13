11346bc96442639a269ea292de02bbd3a1382aa0
Georg Henkel <georg@develman.de>
2012-09-13 17:00:03 +0200

    set dynamic content as own element

--- src/DevelSuite/form/dsForm.php
+++ src/DevelSuite/form/dsForm.php
@@ -304,13 +304,13 @@ class dsForm {
 
 			// add elements
 			foreach ($this->elementList as $element) {
-				#if ($element instanceof dsDynamicContent) {
-				#	$html .= $element->buildHTML();
-				#} else {
+				if ($element instanceof dsDynamicContent) {
+					$html .= $element->buildHTML();
+				} else {
 					$html .= "<li class='dsform-formRow'>\n";
 					$html .= $element->buildHTML();
 					$html .= "</li>\n";
-				#}
+				}
 			}
 
 			$html .= "</ul>\n";
