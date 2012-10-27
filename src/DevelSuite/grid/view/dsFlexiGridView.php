4c6fca39db2ca4600976bd1b3dd6d94e4215c9c4
Georg Henkel <georg@develman.de>
2012-10-27 18:43:45 +0200

    wrong package

--- src/DevelSuite/grid/view/dsFlexiGridView.php
+++ src/DevelSuite/grid/view/dsFlexiGridView.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace DevelSuite\grid\view\impl;
+namespace DevelSuite\grid\view;
 
 use DevelSuite\config\dsConfig;
 use DevelSuite\controller\dsFrontController;
