c4c7d0a7eded0b86d600a1a31432795dd58a5491
Georg Henkel <georg@develman.de>
2013-02-11 22:20:02 +0100

    support different languages

--- src/DevelSuite/view/helper/impl/dsDateViewHelper.php
+++ src/DevelSuite/view/helper/impl/dsDateViewHelper.php
@@ -1,12 +1,18 @@
 <?php
 /*
  * This file is part of the DevelSuite
- * Copyright (C) 2012 Georg Henkel <info@develman.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* Copyright (C) 2012 Georg Henkel <info@develman.de>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 namespace DevelSuite\view\helper\impl;
+
+use DevelSuite\i18n\dsLocale;
+
+use DevelSuite\config\dsConfig;
+
+use DevelSuite\dsApp;
 
 use DevelSuite\view\helper\dsIViewHelper;
 
@@ -19,23 +25,70 @@ use DevelSuite\view\helper\dsIViewHelper;
  */
 class dsDateViewHelper implements dsIViewHelper {
 	/**
+	 * Static definitions of time values for defined languages
+	 */
+	private static $lang_en = array("d" => "days", "h" => "hours", "m" => "minutes", "s" => "seconds");
+	private static $lang_de = array("d" => "Tage", "h" => "Stunden", "m" => "Minuten", "s" => "Sekunden");
+	private static $lang_fr = array("d" => "jours", "h" => "heures", "m" => "minutes", "s" => "secondes");
+	private static $lang_es = array("d" => "días", "h" => "horas", "m" => "minutos", "s" => "segundos");
+	private static $lang_it = array("d" => "giorni", "h" => "orario", "m" => "minuti", "s" => "secondo");
+
+	/**
+	 * Contains definition of time values
+	 * @var array
+	 */
+	private $lang;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$locale = dsConfig::read('app.locale', dsLocale::$UNITED_KINGDOM);
+		switch ($locale) {
+			case dsLocale::$UNITED_KINGDOM:
+				$lang = self::$lang_en;
+				break;
+
+			case dsLocale::$GERMANY:
+				$lang = self::$lang_de;
+				break;
+
+			case dsLocale::$FRANCE:
+				$lang = self::$lang_fr;
+				break;
+					
+			case dsLocale::$SPAIN:
+				$lang = self::$lang_es;
+				break;
+
+			case dsLocale::$ITALY:
+				$lang = self::$lang_it;
+				break;
+		}
+	}
+
+	/**
 	 * Calculate difference off a datetime to current time
 	 * and format it in words (e.g. 1 hour, 2 days, ...)
 	 *
 	 * @param string $time
 	 */
-	public function diffTime($time) {
+	public function diffTime($time, array $lang = NULL) {
+		if ($lang != NULL) {
+			$this->lang = $lang;
+		}
+
 		$todaydate = date("Y-m-d H:i:s");
 
 		$ago = strtotime($todaydate) - strtotime($time);
 		if ($ago >= 86400) {
-			$diff = floor($ago/86400).' days';
+			$diff = floor($ago/86400) . ' ' . $this->lang['d'];
 		} elseif ($ago >= 3600) {
-			$diff = floor($ago/3600).' hours';
+			$diff = floor($ago/3600) . ' ' . $this->lang['h'];
 		} elseif ($ago >= 60) {
-			$diff = floor($ago/60).' minutes';
+			$diff = floor($ago/60) . ' ' . $this->lang['m'];
 		} else {
-			$diff = $ago.' seconds';
+			$diff = $ago . '  ' . $this->lang['s'];
 		}
 
 		return $diff;
