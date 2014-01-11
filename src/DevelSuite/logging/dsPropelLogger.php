e7c82b39080dd1b7961cd65a82f0de2f9575547b
Georg Henkel <georg@develman.de>
2014-01-11 18:55:32 +0000

    add propel logging

---
+++ src/DevelSuite/logging/dsPropelLogger.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the DevelSuite
+ * Copyright (C) 2012 Georg Henkel <info@develman.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DevelSuite\logging;
+
+use Monolog\Logger;
+
+/**
+ * FIXME
+ *
+ * @package DevelSuite\mail
+ * @author  Georg Henkel <info@develman.de>
+ * @version 1.0
+ */
+class dsPropelLogger extends Logger {
+
+    public function emergency($message) {
+        parent::emerg($message);
+    }
+
+    public function warning($message) {
+        parent::warn($message);
+    }
+
+}
