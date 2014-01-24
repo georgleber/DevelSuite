1743ba1c8ba0d1df99c342abd49806decd3b6a91
Georg Henkel <georg@develman.de>
2014-01-24 19:26:01 +0100

    set identifier to allow more than one separaotr

--- src/DevelSuite/grid/action/dsFlexiSeparator.php
+++ src/DevelSuite/grid/action/dsFlexiSeparator.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the DevelSuite
  * Copyright (C) 2012 Georg Henkel <info@develman.de>
@@ -6,9 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace DevelSuite\grid\action;
 
-use DevelSuite\grid\view\dsFlexiGridView;
+namespace DevelSuite\grid\action;
 
 /**
  * Creates a separator entry in the flexigrid action list
@@ -18,28 +18,46 @@ use DevelSuite\grid\view\dsFlexiGridView;
  * @version 1.0
  */
 class dsFlexiSeparator implements dsIFlexiAction {
-	/*
-	 * (non-PHPdoc)
-	 * @see DevelSuite\grid\action.dsIFlexiAction::getIdentifier()
-	 */
-	public function getIdentifier() {
-		return NULL;
-	}
 
-	/*
-	 * (non-PHPdoc)
-	 * @see DevelSuite\grid\action.dsIFlexiAction::getJSFunction()
-	 */
-	public function getJSFunction() {
-		// not needed
-		return "";
-	}
+    /**
+     * Identifier for this action
+     * @var string
+     */
+    private $identifier;
 
-	/*
-	 * (non-PHPdoc)
-	 * @see DevelSuite\grid\action.dsIFlexiAction::__toString()
-	 */
-	public function __toString() {
-		return "{ separator: true }";
-	}
+    /**
+     * Constructor
+     *
+     * @param string $identifier
+     * 		Identifier of this separator
+     */
+    public function __construct($identifier) {
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see DevelSuite\grid\action.dsIFlexiAction::getIdentifier()
+     */
+    public function getIdentifier() {
+        return $this->identifier;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see DevelSuite\grid\action.dsIFlexiAction::getJSFunction()
+     */
+    public function getJSFunction() {
+        // not needed
+        return "";
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see DevelSuite\grid\action.dsIFlexiAction::__toString()
+     */
+    public function __toString() {
+        return "{ separator: true }";
+    }
+
 }
